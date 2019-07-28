eb771eb0446f1faaeaa9892f60204f65495db38a
Noxy <marcin.malysz@explaineverything.com>
2019-07-28 15:04:51 +0200

    basic setup

--- platform/Android/app/src/main/cpp/native-lib.cpp
+++ platform/Android/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,11 @@
-#include <jni.h>
+#include "native-lib.h"
+
+#include <stdint.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_noclip_marcinmalysz_sagacross_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
+JNIEXPORT jstring JNICALL Java_com_noclip_marcinmalysz_sagacross_MainActivity_stringFromJNI( JNIEnv* env, jobject /* this */) {
+
     std::string hello = "Hello from C++";
+
     return env->NewStringUTF(hello.c_str());
 }
