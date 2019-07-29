98f45604a6fec6bafd3589bea6e19aa48a9a233d
Noxy <marcin.malysz@explaineverything.com>
2019-07-29 11:40:11 +0200

    Android ndk engine final, cmake script copy

--- platform/Android/app/src/main/cpp/native-lib.h
+++ platform/Android/app/src/main/cpp/native-lib.h
@@ -5,7 +5,13 @@
 
 extern "C" {
 
-JNIEXPORT jstring JNICALL Java_com_noclip_marcinmalysz_sagacross_MainActivity_stringFromJNI(JNIEnv* env, jobject /* this */);
+JNIEXPORT void JNICALL Java_com_noclip_marcinmalysz_sagacross_SCGameWrapper_initializeEngine(JNIEnv* env, jobject object, jint width, jint height, jstring path);
+JNIEXPORT void JNICALL Java_com_noclip_marcinmalysz_sagacross_SCGameWrapper_renderGame(JNIEnv *env, jobject object);
+JNIEXPORT void JNICALL Java_com_noclip_marcinmalysz_sagacross_SCGameWrapper_handleTouch(JNIEnv *env, jobject object, jfloat x, jfloat y, jboolean selected);
+JNIEXPORT void JNICALL Java_com_noclip_marcinmalysz_sagacross_SCGameWrapper_handleMovement(JNIEnv *env, jobject object, jfloat dx, jfloat dy, jfloat angle);
+JNIEXPORT void JNICALL Java_com_noclip_marcinmalysz_sagacross_SCGameWrapper_fireBullet(JNIEnv *env, jobject object);
+JNIEXPORT void JNICALL Java_com_noclip_marcinmalysz_sagacross_SCGameWrapper_dropMine(JNIEnv *env, jobject object);
+JNIEXPORT jboolean JNICALL Java_com_noclip_marcinmalysz_sagacross_SCGameWrapper_renderingGame(JNIEnv *env, jobject object);
 
 }
 
--- platform/Android/app/src/main/java/com/noclip/marcinmalysz/sagacross/MainActivity.kt
+++ platform/Android/app/src/main/java/com/noclip/marcinmalysz/sagacross/MainActivity.kt
@@ -2,23 +2,17 @@ package com.noclip.marcinmalysz.sagacross
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val wrapper: SCGameWrapper = SCGameWrapper()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
-    }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+    }
 
     companion object {
 
